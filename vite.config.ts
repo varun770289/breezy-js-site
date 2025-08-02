e6a9e999b80086da9b8e8784d1c3d5c8fc00131a
varun770289 <karnamvarun7702@gmail.com>
2025-08-03 02:01:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 5173,
+    port: 5174,
   },
   plugins: [
     react(),
